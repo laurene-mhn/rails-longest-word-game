b50f578ff7cefe534ab60ae0a1696ba8f20820b6
laurene michon <laurene.michon@gmail.com>
2020-11-09 19:00:33 +0100

    Letters ex ok

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,5 @@
+require "open-uri"
+
 class GamesController < ApplicationController
   def new
     @letters = Array.new(9) { ('A'..'Z').to_a.sample }
@@ -6,6 +8,24 @@ class GamesController < ApplicationController
 
   def score
     # raise
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
   end
 
+  private
+
+  def included?(word, letters)
+    word.chars.all? do |letter| 
+      # binding.pry
+      word.count(letter) <= letters.count(letter)
+    end
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    return json['found']
+  end
 end
